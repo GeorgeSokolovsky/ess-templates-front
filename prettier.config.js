215e2b024402a21001d290c4dbfeafcd16c0941d
George Sokolovsky <georgesokolovsky@gmail.com>
2019-07-24 21:33:08 +0300

    prettier format

---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+const prettierConfig = require('@tinkoff/linters/prettier/prettier.config');
+
+module.exports = {...prettierConfig, trailingComma: 'none'};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'ess-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'ess-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ess-templates-angular';
+    title = 'ess-templates-angular';
 }
