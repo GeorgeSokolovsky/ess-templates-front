b7a1ce2005fa246651d0d82fc3892f77c145818e
George Sokolovsky <georgesokolovsky@gmail.com>
2019-07-24 21:21:08 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ess-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ess-templates-angular';
+}
