23082e3fb395f803fa1bbbc7ac87ce5506cc2729
George Sokolovsky <georgesokolovsky@gmail.com>
2019-08-02 19:36:40 +0300

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,16 +23,4 @@ describe('AppComponent', () => {
 
         expect(app.title).toEqual('ess-templates-angular');
     });
-
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-
-        fixture.detectChanges();
-
-        const compiled = fixture.debugElement.nativeElement;
-
-        expect(compiled.querySelector('h1').textContent).toContain(
-            'Welcome to ess-templates-angular!'
-        );
-    });
 });
